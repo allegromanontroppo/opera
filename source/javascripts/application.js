98d9464de8bb1645cff80a6879948f96cbddded1
Mark Holland <rennerholland@googlemail.com>
2013-11-29 01:16:33 +0000

    foundation 5

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,18 +10,18 @@
 
 // or pick and choose
 //= require foundation/foundation
-// require foundation/foundation.alerts
+// require foundation/foundation.abide
+// require foundation/foundation.accordion
+// require foundation/foundation.alert
 // require foundation/foundation.clearing
-// require foundation/foundation.cookie
 // require foundation/foundation.dropdown
-// require foundation/foundation.forms
 // require foundation/foundation.joyride
 // require foundation/foundation.magellan
+//= require foundation/foundation.offcanvas
 // require foundation/foundation.orbit
-// require foundation/foundation.placeholder
 // require foundation/foundation.reveal
-// require foundation/foundation.section
-// require foundation/foundation.tooltips
+// require foundation/foundation.tab
+// require foundation/foundation.tooltip
 //= require foundation/foundation.topbar
 
 //= require billboard_and_headliners
