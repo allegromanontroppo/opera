3ebde96e15841d3d378d313bd6535bafe73762d9
Mark Holland <rennerholland@googlemail.com>
2014-07-10 23:09:26 +0100

    serif font, video updates

--- helpers/opera_helpers.rb
+++ helpers/opera_helpers.rb
@@ -78,7 +78,7 @@ module OperaHelpers
     hash_builder = lambda{ |*args|
       {
         :name      => args.last.name, 
-        :url       => build_path(*args)[1..-1], 
+        :url       => build_path(*args)[1..-6], 
         :image_src => args.last.image_src[1..-1]
       }
     }
